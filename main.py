8b98ca2e50d0911023ba9e69d62bbc1c49f3271f
James0103 <jameshhjung@gmail.com>
2025-11-29 13:49:45 +0900

    251129_4_default_chatbot_example

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-import uvicorn
+import os
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import PlainTextResponse, HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 
 app = FastAPI()
 
+# 프론트엔드 파일 서빙
 @app.get("/")
 def index():
-    return PlainTextResponse("Daily Challenge", status_code=200)
+  return FileResponse("static/index.html")
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", reload=False)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 웹소켓 부분
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+  await websocket.accept()
+  while True:
+      data = await websocket.receive_text()
+      await websocket.send_text(f"Message text was: {data}")
